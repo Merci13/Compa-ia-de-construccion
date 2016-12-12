02721eeb9f08aa1b0efe1b979f204bb93e3c1087
Merci13 <mrjorxe@gmail.com>
2016-12-12 01:58:26 -0600

    Update logicaedita.js

--- logicaedita.js
+++ logicaedita.js
@@ -30,10 +30,39 @@ if (id>-1) {
 
 	console.log(datos[id].nombreProjecto.nombre);
 	$("#editarProjecto").text(datos[id].nombreProjecto.nombre);
-	for (var i = 0; i < datos[id].length; i++){
-$('#porHacer').append('<div id="'+guid()+'" draggable="true" ondragstart="drag(event)"><button class="btn btn-default" type="submit">'+datos[id].nombreProjecto.tareas[2]+'</button><br></div>');
+	for (var i = 0; i < datos[id].nombreProjecto.length; i++){
+		$('#porHacer').append('<div id="'+guid()+'" draggable="true" ondragstart="drag(event)"><button class="btn btn-default" type="submit">'+datos[id].nombreProjecto.tareas[2]+'</button><br></div>');
 		
 	}
+
+	var porhacer=0;
+	var enprogreso=0;
+	var finalizados=0;
+	
+	for (var i = 0; i < datos[id].nombreProjecto.tareas.length; i++) {
+		datos[id].nombreProjecto.tareas.length;
+		if (datos[id].nombreProjecto.tareas[i].orden==1) {
+			porhacer++;
+		}else if (datos[id].nombreProjecto.tareas[i].orden==2) {
+			enprogreso++;
+		}else{
+			finalizados++;
+		};
+
+		
+
+	};
+var data = [
+        { name: 'Por Hacer', y: porhacer },
+        {name: 'En progreso', y: enprogreso},
+        {name: 'Finalizadas', y: finalizados },
+       
+    ];
+var texto= "Tareas Por Proyecto";
+draw_chart_tareas_proyecto('graficio_tareas_por_proyecto',data);
+
+
+
 }else{
 	window.location.href ="./index.html";
 
@@ -47,6 +76,109 @@ $("#AgregarPersona").click(function(event) {
 
 	var nombrePersona=$("#nombre_nuevo_persona").val();
 	datos[id].nombreProjecto.personas.push({'id':datos[id].nombreProjecto.personas, 'nombre': nombrePersona});
+	var personanueva=$("#nombre_nuevo_persona").val();
+	
+	console.log(datos[id].nombreProjecto.personas);
+
+
+		Persister.saveObj('projectos',datos);
+		$('#colaborador').append('<div id="" ><button class="btn btn-default" type="submit">'+personanueva+'</button><br></div>');
+		
+	
+		});
+
+});
+
+/*
+Metodo para agregar tareas
+*/
+$("#AgregarTarea").click(function(event) {
+
+	var tarea=$("#nombre_nuevo_tarea").val();
+	datos[id].nombreProjecto.tareas.push({'id':datos[id].nombreProjecto.tareas.length, 'tarea': tarea,'orden':1});
+	console.log(datos[id].nombreProjecto.tareas);
+
+
+		Persister.saveObj('projectos',datos);
+		console.log(datos);
+
+		$('#porHacer').append('<div id="'+guid()+'" draggable="true" ondragstart="drag(event)"><button class="btn btn-default" type="submit">'+tarea+'</button><br></div>');
+		
+	
+		});
+
+
+
+//Metodos para el drag and drop
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
+//metodo para dibujar el grafico
+function draw_chart_tareas_proyecto(selector, data) {
+	
+	Highcharts.chart(selector, {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Tareas del Proyecto'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: data
+        }]
+    });
+}
+
+
+
+
+
+
+
+
 	var personanueva=$("#nombre_nuevo_persona").val();
 	
 	console.log(datos[id].nombreProjecto.personas);
